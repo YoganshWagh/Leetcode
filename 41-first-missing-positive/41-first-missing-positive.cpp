2e4f068e36235b9a5aa272066f80e55bac14359f
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-14 00:23:54 +0530

    Time: 809 ms (5.09%), Space: 139 MB (6.47%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,18 +1,27 @@
 class Solution {
 public:
     int firstMissingPositive(vector<int>& nums) {
-       unordered_set<int> set;
-        for(int i=0;i<nums.size();i++){
-            set.insert(nums[i]);
+        // set.find(k)==set.end()
+       //   set.find()!=set.end()
+        set<int> sett;
+        for( int i=0; i<nums.size(); ++i )
+        {
+            sett.insert( nums[i] );
         }
-        // 0 1 2 
-        int k = 1;
-       while(1){
-           if(set.find(k)==set.end()){
-               return k;
-           }
-           k++;
-       }
+        
+        int c = 1;
+        while(true)
+        {
+            if( sett.find(c) != sett.end() )
+            {
+                c++;
+            }
+            else
+            {
+                return c;
+            }
+        }
+        
         return 0;
     }
 };
