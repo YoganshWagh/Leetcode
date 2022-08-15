430be018ad92d835299bf93ba1aefb713cb47236
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-16 00:05:03 +0530

    Time: 8 ms (58.84%), Space: 7.9 MB (7.43%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -1,36 +1,18 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        
-        string c = "";
-        for( char i : s )
-        {
-            if( i >='A' && i<='Z' )
-            {
-                c += tolower(i);
-            }
-            else if( i>='a' && i<='z' )
-            {
-                c += i;
-            }
-            else if( i>='0' && i<='9' )
-            {
-                c += i;
+        string str;
+        for(int i=0;i<s.size(); ++i){
+            if(s[i]>='A' && s[i]<='Z' || s[i]>='a' && s[i]<='z'|| s[i]>='0' && s[i]<='9'){
+                str.push_back(s[i]);
             }
         }
-        
-        int i = 0, j = c.size() - 1;
-        while(i<j)
-        {
-            if( c[i] != c[j] )
-            {
-                return false;
-            }
-            i++;
-            j--;
+        transform(str.begin(),str.end(),str.begin(),::tolower);
+        string ans = str;
+        reverse(ans.begin(),ans.end());
+        if(ans==str){
+            return true;
         }
-        
-        return true;
+        return false;
     }
 };
-  
