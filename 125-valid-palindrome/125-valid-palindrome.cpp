cf45b26d688b356480872bd6e6aa29bd71b71e9f
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-16 00:07:01 +0530

    Time: 11 ms (36.52%), Space: 7.8 MB (24.57%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.cpp
+++ 125-valid-palindrome/125-valid-palindrome.cpp
@@ -3,7 +3,13 @@ public:
     bool isPalindrome(string s) {
         string str;
         for(int i=0;i<s.size(); ++i){
-            if(s[i]>='A' && s[i]<='Z' || s[i]>='a' && s[i]<='z'|| s[i]>='0' && s[i]<='9'){
+            if(s[i]>='A' && s[i]<='Z'){
+                str.push_back(s[i]);
+            }
+            if(s[i]>='a' && s[i]<='z'){
+                str.push_back(s[i]);
+            }
+            if(s[i]>='0' && s[i]<='9'){
                 str.push_back(s[i]);
             }
         }
