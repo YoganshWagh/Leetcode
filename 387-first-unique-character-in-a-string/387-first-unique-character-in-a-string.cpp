6a3660a37af8023b0e04fc47b00177ffd8ad1021
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-16 16:07:00 +0530

    Time: 82 ms (26.81%), Space: 10.9 MB (15.57%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -7,12 +7,15 @@ public:
         {
             mp[x]++;
         }
-        for(int i=0;i<s.size();++i){
-            if(mp[s[i]]==1){
+        
+        for( int i = 0; i<s.size(); ++i )
+        {
+            if( mp[s[i]] == 1 )
+            {
                 return i;
             }
         }
-        return -1;
         
+        return -1;
     }
 };
