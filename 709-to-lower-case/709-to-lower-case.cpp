e6cda9f7d4d6b107311a6a84c3f43379648c6242
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-17 18:27:30 +0530

    Time: 0 ms (100.00%), Space: 6.2 MB (8.69%) - LeetHub

--- 709-to-lower-case/709-to-lower-case.cpp
+++ 709-to-lower-case/709-to-lower-case.cpp
@@ -1,9 +1,15 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        
-        transform(s.begin(),s.end(),s.begin(),::tolower);
+ 
+        for( int i = 0; i<s.size(); ++i )
+        {
+            if( s[i] >= 65 && s[i] <= 90 )
+            {
+                s[i] = s[i] + 32;
+            }
+           
+        }
         return s;
-        
     }
 };
