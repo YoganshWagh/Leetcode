cffb978630e35201c1db35d11d6077efaf72d347
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-20 18:14:14 +0530

    Time: 3 ms (95.69%), Space: 11.8 MB (15.98%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,17 +1,65 @@
+int firstOccurence( vector<int> &nums, int target )
+{
+    int s = 0, e = nums.size()-1;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    while( s<=e )
+    {
+        if( nums[mid] == target )
+        {
+            ans = mid;
+            e = mid - 1;
+        }
+        else if( nums[mid] > target )
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+        
+    }
+    return ans;
+}
+
+int secondOccurence( vector<int> &nums, int target )
+{
+    int s = 0, e = nums.size()-1;
+    int ans = -1;
+    int mid = s + (e-s)/2;
+    while( s<=e )
+    {
+        if( nums[mid] == target )
+        {
+            ans = mid;
+            s = mid + 1;
+        }
+        else if( nums[mid] > target )
+        {
+            e = mid - 1;
+        }
+        else
+        {
+            s = mid + 1;
+        }
+        mid = s + (e-s)/2;
+        
+    }
+    return ans;
+}
 class Solution {
 public:
     vector<int> targetIndices(vector<int>& nums, int target) {
-       
-        // 1,2,2,3,5
+        
         sort( nums.begin(), nums.end() );
         vector<int> v;
-        for( int i = 0; i<nums.size(); ++i )
-        {
-            if( nums[i] == target )
-            {
-                v.push_back(i);
-            }
-        }
+        int i = secondOccurence(  nums,  target );
+        int j = firstOccurence( nums,  target );
+        if(i==-1 && j==-1) return v;
+        for(int x = j;x<=i; x++) v.emplace_back(x);
         return v;
+        
     }
 };
