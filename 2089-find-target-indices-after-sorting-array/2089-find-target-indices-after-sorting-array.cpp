0ac70e7dfbc3d45ea91282de4f1a94af2ef16ced
YoganshWagh <104297129+YoganshWagh@users.noreply.github.com>
2022-08-20 18:32:37 +0530

    Time: 8 ms (62.93%), Space: 11.5 MB (84.77%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -1,64 +1,64 @@
-int firstOccurence( vector<int> &nums, int target )
-{
-    int s = 0, e = nums.size()-1;
-    int ans = -1;
-    int mid = s + (e-s)/2;
-    while( s<=e )
-    {
-        if( nums[mid] == target )
-        {
-            ans = mid;
-            e = mid - 1;
-        }
-        else if( nums[mid] > target )
-        {
-            e = mid - 1;
-        }
-        else
-        {
-            s = mid + 1;
-        }
-        mid = s + (e-s)/2;
-        
-    }
-    return ans;
-}
-
-int secondOccurence( vector<int> &nums, int target )
-{
-    int s = 0, e = nums.size()-1;
-    int ans = -1;
-    int mid = s + (e-s)/2;
-    while( s<=e )
-    {
-        if( nums[mid] == target )
-        {
-            ans = mid;
-            s = mid + 1;
-        }
-        else if( nums[mid] > target )
-        {
-            e = mid - 1;
-        }
-        else
-        {
-            s = mid + 1;
-        }
-        mid = s + (e-s)/2;
-        
-    }
-    return ans;
-}
 class Solution {
 public:
+   // vector<int> occ;
+    void Occurence( vector<int> &nums, int target,vector<int> &occ )
+   {
+    int s = 0, e = nums.size()-1;
+    int ans = -1;
+    int midd = s + (e-s)/2;
+    while( s<=e )
+    {
+        if( nums[midd] == target )
+        {
+            ans = midd;
+            e = midd - 1;
+        }
+        else if( nums[midd] > target )
+        {
+            e = midd - 1;
+        }
+        else
+        {
+            s = midd + 1;
+        }
+        midd = s + (e-s)/2;
+        
+    }
+    occ[0]=ans;
+    int ss = 0, ee = nums.size()-1;
+    ans = -1;
+    int mid = ss + (ee-ss)/2;
+    while( ss<=ee )
+    {
+        if( nums[mid] == target )
+        {
+            ans = mid;
+            ss = mid + 1;
+        }
+        else if( nums[mid] > target )
+        {
+            ee = mid - 1;
+        }
+        else
+        {
+            ss = mid + 1;
+        }
+        mid = ss + (ee-ss)/2;
+        
+    }
+    occ[1]=ans;
+}
     vector<int> targetIndices(vector<int>& nums, int target) {
         
-        sort( nums.begin(), nums.end() );
+        sort( nums.begin(), nums.end());
         vector<int> v;
-        int i = secondOccurence(  nums,  target );
-        int j = firstOccurence( nums,  target );
-        if(i==-1 && j==-1) return v;
-        for(int x = j;x<=i; x++) v.emplace_back(x);
+        vector<int > occ(2);
+        Occurence(nums, target,occ);
+        if(occ[0]==-1 && occ[1]==-1) {
+             occ.clear();
+             return occ;
+        }
+        for(int i=occ[0];i<=occ[1]; i++) v.push_back(i);
         return v;
         
     }
