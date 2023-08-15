788ef8b21c0d1fc0a01f7ff0b89ad456b2f38b05
YOGANSH WAGH <104297129+YoganshWagh@users.noreply.github.com>
2023-08-16 00:32:57 +0530

    Create 1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
    Given an array nums, return true if the array was originally sorted in non-decreasing order, then rotated some number of positions (including zero). Otherwise, return false.
    
    There may be duplicates in the original array.
    
    Note: An array A rotated by x positions results in an array B of the same length such that A[i] == B[(i+x) % A.length], where % is the modulo operation.
    
     
    
    Example 1:
    
    Input: nums = [3,4,5,1,2]
    Output: true
    Explanation: [1,2,3,4,5] is the original sorted array.
    You can rotate the array by x = 3 positions to begin on the the element of value 3: [3,4,5,1,2].
    Example 2:
    
    Input: nums = [2,1,3,4]
    Output: false
    Explanation: There is no sorted array once rotated that can make nums.
    Example 3:
    
    Input: nums = [1,2,3]
    Output: true
    Explanation: [1,2,3] is the original sorted array.
    You can rotate the array by x = 0 positions (i.e. no rotation) to make nums.
     
    
    Constraints:
    
    1 <= nums.length <= 100
    1 <= nums[i] <= 100

---
+++ 1752_Check_if_Array_Is_Sorted_and_Rotated.cpp
@@ -0,0 +1,23 @@
+// https://leetcode.com/problems/check-if-array-is-sorted-and-rotated/description/
+
+class Solution {
+public:
+    bool check(vector<int>& nums) {
+        
+        int n = nums.size();
+        int count = 0;
+
+        for( int i=1; i<n; i++ )
+        {
+            if( nums[i-1] > nums[i] )
+                count++;
+        }
+
+        //FOR ONE CASE OF COMPARING 0TH ELEMENT AND N-1TH ELEMENT:
+        if( nums[n-1] > nums[0] )
+            count++;
+
+        return count<=1;
+
+    }
+};
