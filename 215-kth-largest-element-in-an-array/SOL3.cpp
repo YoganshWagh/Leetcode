81b5d576ec30da3fbb4c875bbab709e437f33701
YOGANSH WAGH <yogansh04@gmail.com>
2023-10-10 00:03:00 +0530

    Runtime 116ms Beats 11.50%of users with C++, Memory 57.92MB Beats 14.51%of users with C++

---
+++ 215-kth-largest-element-in-an-array/SOL3.cpp
@@ -0,0 +1,20 @@
+//BEST SOLUTION USING MIN HEAP:
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        
+        //BEST SOLUTION USING MIN HEAP:
+        //MIN HEAP CREATION:
+        priority_queue< int, vector<int>, greater<int> > pq;
+
+        for( auto x : nums )
+        {
+            pq.push(x);
+            if( pq.size() > k )
+                pq.pop();
+        }
+
+        return pq.top();
+    }
+};
