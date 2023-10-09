83df065e24045f87664f37587cc2626ca0cb93ee
YOGANSH WAGH <yogansh04@gmail.com>
2023-10-10 01:33:01 +0530

    Runtime 4ms Beats 44.61%of users with C++ Memory 7.52MB Beats 87.82%of users with C++

--- 412-fizz-buzz/CODE.cpp
+++ 412-fizz-buzz/CODE.cpp
@@ -5,7 +5,7 @@ public:
     vector<string> fizzBuzz(int n) {
         
         vector<string> ans(n);
-        for( int i=1; i<n+1; i++ )
+        for( int i=1; i<=n; i++ )
         {
             if( i%3==0 && i%5==0 ) ans[i-1] = "FizzBuzz";
             else if( i%3==0 ) ans[i-1] = "Fizz";
