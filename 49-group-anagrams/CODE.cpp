2bb88c1862c6bab3749d3feb06f6641dd6fd9c6a
YOGANSH WAGH <yogansh04@gmail.com>
2023-10-11 11:39:53 +0530

    Runtime 21ms Beats 95.07%of users with C++, Memory 21.31MB Beats 26.03%of users with C++

---
+++ 49-group-anagrams/CODE.cpp
@@ -0,0 +1,24 @@
+//QUESTION: https://leetcode.com/problems/group-anagrams/description/
+//SOLUTION: https://leetcode.com/problems/group-anagrams/solutions/3687735/beats-100-c-java-python-beginner-friendly/
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        //FOR STORING ANAGRAMS AND THEIR ORIGINAL FORM:
+        unordered_map<string, vector<string>> mp;
+        
+        for( auto x : strs )
+        {
+            string word = x;
+            sort( word.begin(), word.end() );
+            mp[word].push_back(x);
+        }
+
+        //TO STORE GROUPED ANAGRAMS:
+        vector< vector<string> > ans;
+        for( auto x : mp )
+            ans.push_back(x.second);
+
+        return ans;
+    }
+};
