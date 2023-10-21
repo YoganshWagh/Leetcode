4fb3552bbeb0e63a955110b458f4c794f68d4d29
YOGANSH WAGH <yogansh04@gmail.com>
2023-10-21 23:45:59 +0530

    Runtime 0 ms Beats 100%, Memory 9.2 MB Beats 17.59%

---
+++ 66-plus-one/CODE.cpp
@@ -0,0 +1,26 @@
+//QUESTION: https://leetcode.com/problems/plus-one/
+//SOLUTION: https://leetcode.com/problems/plus-one/solutions/3054321/simple-c-solution-with-good-explanation/
+
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& v) {
+
+        int n = v.size();
+        for(int i = n-1; i >= 0; i--)
+        {
+            if(i == n-1)
+                v[i]++;
+            if(v[i] == 10){
+                v[i] = 0;
+                if(i != 0){
+                    v[i-1]++;
+                }
+                else{
+                    v.push_back(0);
+                    v[i] = 1;
+                }
+            }
+        }
+        return v;
+    }
+};
