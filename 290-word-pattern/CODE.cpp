4f597c892271b0b3a27d231ab8b38f43d7fc66ad
YOGANSH WAGH <yogansh04@gmail.com>
2023-11-01 22:54:18 +0530

    Runtime 0ms Beats 100.00%, Memory 6.71MB Beats 55.38%+

---
+++ 290-word-pattern/CODE.cpp
@@ -0,0 +1,33 @@
+// QUESTION: https://leetcode.com/problems/word-pattern/description/
+// SOLUTION: https://youtu.be/XO4U2KWgIgY?si=Dn-SXmh3jSWNQLnm
+
+class Solution {
+   
+public:
+    bool wordPattern(string pattern, string s) {
+        unordered_map<char,string>mp;
+        unordered_map<string,char>mp2;
+        int j =0;
+        int i =0;
+        while( i<pattern.length() && j<s.length()){
+         string str = "";
+
+        while(j<s.length() && s[j]!=' '){
+          str+=s[j];
+          j++;
+        }
+        j++;
+        if(mp.count(pattern[i]))
+             if(mp[pattern[i]]!=str)return false;
+         if(mp2.count(str))
+          if(mp2[str]!=pattern[i])return false;
+         //else 
+             mp[pattern[i]]=str;
+             mp2[str]=pattern[i];
+         i++;
+      }
+      if(j<s.length())return false;
+       if(i<pattern.length())return false;
+      return true;
+    }
+};
