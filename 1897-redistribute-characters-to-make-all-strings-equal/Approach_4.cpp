d255debbaa4ac6b6c124aac275c29eb512959522
YOGANSH WAGH <yogansh04@gmail.com>
2023-12-30 22:41:52 +0530

    Runtime 23 ms Beats 10.12%, Memory 12.68 MB Beats 21.01%

---
+++ 1897-redistribute-characters-to-make-all-strings-equal/Approach_4.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool makeEqual(vector<string>& words) {
+        
+        //Approach 4: Using STL only lamda written in same line.
+        int count[26] = {0};
+        int n = words.size();
+        for( string x : words )
+            for( char y : x ) count[y -'a']++;
+
+        //all_of() ensures that all elements meet the given
+        //condition or not.
+        //& is used to access all outside values in lambda function.
+        return all_of( begin(count), end(count), [&](int freq){
+            return freq % n == 0;
+        });
+    }
+};
