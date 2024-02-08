f3faa6bc82de5884688495045a51743f981f3ad6
YOGANSH WAGH <yogansh04@gmail.com>
2024-02-08 22:56:07 +0530

    Runtime 43 ms Beats 88.64%, Memory 10.45 MB Beats 38.33%

---
+++ 279-perfect-squares/Approach_2.cpp
@@ -0,0 +1,21 @@
+//Approach 2: Using Bottom Up.
+//T.C = O(n^2), the total number of subproblems solved is proportional to the sum of the first n square numbers. This sum can be approximated as O(n^2).
+//S.C = O(n), where the space required for the memoization array is O(n), since it stores solutions for each value from 0 to n.
+
+class Solution {
+public:
+    int numSquares(int n) 
+    {
+        vector<int> t(n+1, INT_MAX);
+        //t[i] = number of perfect squares to get i.
+        //return t[n].
+
+        t[0] = 0; //since we are not gonna use '0'.
+        for(int i=1; i <= n; i++)
+        {
+            for(int j=1; j*j <= i; j++)
+                t[i] = min(t[i], 1 + t[i - j*j]);
+        }
+        return t[n];
+    }
+};
