439ea324243642220480883498dba5c7749aaac4
YOGANSH WAGH <yogansh04@gmail.com>
2024-02-08 22:58:43 +0530

    Runtime 43 ms Beats 88.64%, Memory 10.45 MB Beats 38.33%

--- 279-perfect-squares/Approach_2.cpp
+++ 279-perfect-squares/Approach_2.cpp
@@ -1,4 +1,5 @@
 //Approach 2: Using Bottom Up.
+//REFERENCE: https://youtu.be/7zfmLa6vzaA
 //T.C = O(n^2), the total number of subproblems solved is proportional to the sum of the first n square numbers. This sum can be approximated as O(n^2).
 //S.C = O(n), where the space required for the memoization array is O(n), since it stores solutions for each value from 0 to n.
 
