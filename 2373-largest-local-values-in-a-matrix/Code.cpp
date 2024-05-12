c0b81207e37521ec337a3a46869b8303a1b73e13
YOGANSH WAGH <yogansh04@gmail.com>
2024-05-13 00:00:58 +0530

    Runtime 7 ms Beats 89.68%, Memory 13.36 MB Beats 96.76%

---
+++ 2373-largest-local-values-in-a-matrix/Code.cpp
@@ -0,0 +1,30 @@
+//T.C = O(n*n)
+//S.C = O(1)
+class Solution {
+public:
+    vector<vector<int>> largestLocal(vector<vector<int>>& grid) {
+        int row = grid.size();
+        int col = grid[0].size();
+        
+        for(int i=0; i<row-2; i++){
+            for(int j=0; j<col-2; j++){
+                
+                int maxLocal = INT_MIN;
+
+                for(int k=0; k<3; k++){
+                    for(int p=0; p<3; p++){
+                        maxLocal = max(maxLocal, grid[i+k][j+p]);
+                    }
+                }
+                grid[i][j] = maxLocal;
+            }
+        }
+
+        //FOR DOING INPLACE WE ARE CHANGING SIZE OF ARRAY TO 'row-2'
+        grid.resize(row - 2);
+        for(int i=0; i<row; i++)
+            grid[i].resize(row-2);
+        
+        return grid;
+    }
+};
