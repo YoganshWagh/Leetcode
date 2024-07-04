63536db33d40a79a06d3241a241fe2c00b7ac251
YOGANSH WAGH <yogansh04@gmail.com>
2024-07-04 21:53:19 +0530

    Runtime 16 ms Beats 24.04%, Memory 12.92 MB Beats 11.00%

--- 13-roman-to-integer/CODE.cpp
+++ 13-roman-to-integer/CODE.cpp
@@ -1,11 +1,13 @@
-//https://leetcode.com/problems/roman-to-integer/
+//QUESTION: https://leetcode.com/problems/roman-to-integer/
 //SOLUTION: https://leetcode.com/problems/roman-to-integer/solutions/3651672/best-method-c-java-python-beginner-friendly/
-
+//Approach 1: Using Hash Table.
+//T.C = O(n), where n is the length of input string 's'.
+//S.C = O(1), since no extra amount of space is used.
 class Solution {
 public:
     int romanToInt(string s) {
         
-        unordered_map<char ,int> mp;
+        unordered_map<char, int> mp;
 
         mp['I'] = 1;
         mp['V'] = 5;
