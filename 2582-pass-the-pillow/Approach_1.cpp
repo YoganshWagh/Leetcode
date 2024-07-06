a61f975f49f85a1df037aa69b50b1b5e61a7d56a
YOGANSH WAGH <yogansh04@gmail.com>
2024-07-06 20:09:11 +0530

    Runtime 0 ms Beats 100.00%, Memory 7.29 MB Beats 30.58%

---
+++ 2582-pass-the-pillow/Approach_1.cpp
@@ -0,0 +1,26 @@
+//QUESTION: https://leetcode.com/problems/pass-the-pillow/description/
+//Approach 1: Using Iterative approach.
+//T.C = O(n), where 'n' is input given.
+//S.C = O(1), since no extra space is used.
+class Solution {
+public:
+    int passThePillow(int n, int time) 
+    {
+        int dir = 1;
+        int i = 1;
+        int j = 0;
+
+        while(j != time){
+            if(dir == 1){
+                i++;
+            }else{
+                i--;
+            }
+
+            j++;
+            if(i == n) dir = -1;
+            if(i == 1) dir = 1;
+        }
+        return i;
+    }
+};
